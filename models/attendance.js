eb026360ffa0874479fe99db7c88ae9e3014f9e9
SirTwirl <j.kruk123@o2.pl>
2026-09-23 11:42:17 +0200

    feat(models): add Attendance schema

---
+++ models/attendance.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const attendanceSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['present', 'absent'],
+        required: true
+    }
+})
+
+attendanceSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+attendanceSchema.index({ user: 1, date: 1 }, { unique: true })
+
+module.exports = mongoose.model('Attendance', attendanceSchema)
