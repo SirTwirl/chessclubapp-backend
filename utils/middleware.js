dc8bb6c31b2be95a48f8a5cd849fae6d65389e25
SirTwirl <j.kruk123@o2.pl>
2026-09-24 11:32:15 +0200

    refactor(middleware): add auth & RBAC middleware and update app config

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,7 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const config = require('./config')
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -22,13 +25,56 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'MongoServerError' && error.code === 11000) {
     const fields = error.keyValue ? Object.keys(error.keyValue).join(', ') : 'field'
     return response.status(400).json({ error: `${fields} must be unique` })
-  }
+  } else if(error.name === 'JsonWebTokenError') {
+    return response.status(401).json({error: 'invalid token'})
+  } else if (error.name === 'TokenExpiredError') {
+  return response.status(401).json({ error: 'token expired' })
+}
 
   next(error)
+}
+
+const userExtractor = async (request, response, next) => {
+  if(request.token) {
+    const decodedToken = jwt.verify(request.token, config.SECRET)
+    if(decodedToken.id) {
+      request.user = await User.findById(decodedToken.id)
+    }
+  } else {
+    request.user = null
+  }
+  next()
+}
+
+const tokenExtractor = async (request, response, next) => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+  next()
+}
+
+const requireRole = (...allowedRoles) => {
+  return (request, response, next) => {
+    if (!request.user) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    if (!allowedRoles.includes(request.user.role)) {
+      return response.status(403).json({ error: 'You do not have permission to fetch users' })
+    }
+
+    next()
+  }
 }
 
 module.exports = {
   unknownEndpoint,
   requestLogger,
-  errorHandler
+  errorHandler,
+  userExtractor,
+  tokenExtractor,
+  requireRole
 }
